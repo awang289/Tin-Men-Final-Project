13f23f06d73fd0600de03f3a8afb94fcde538df3
awang289 <awang289@gmail.com>
2014-01-13 13:45:11 -0500

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,3 +1,8 @@
 public abstract class Player {
   protected String name;
+  protected ArrayList<Card> hand;
+  protected int score;
+  public int getScore { return score; }
+  public String getName { return name; }
+  public String toString { }
 }
