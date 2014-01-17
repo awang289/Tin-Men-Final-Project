444c397dd86fae77bbaceaef600b1df89379faf0
kyleoleksiuk <kyleoleksiuk@gmail.com>
2014-01-17 13:54:15 -0500

    Update Human.java

--- Human.java
+++ Human.java
@@ -3,9 +3,9 @@ public class Human extends Player{
   public String isOkay(Card d){
     
     String retStr = "";
-    //add isBroken, isLeading, _getSuitName
-    if ( isLeading == true && isBroken == false) {retStr = "Illegal move. Hearts has not yet been broken, please lead a card or another suit.";}
-    if (isLeading == false && _trickSuit != d._getSuit()){retStr = "Illegal move. Please play a " + d._getSuitName + ".";} //make getSuitName
+    
+    if ( isLeading == true && isBroken == false) {retStr = "Illegal move. Hearts has not yet been broken, please lead a card of another suit.";}
+    if (isLeading == false && _trickSuit != d.getSuit()){retStr = "Illegal move. Please play a " + d.getSuitName() + ".";} //make getSuitName
     if (inHand(d) == false){retStr = "Illegal move. Please select a card in your hand.";}
     
     return retStr;   
