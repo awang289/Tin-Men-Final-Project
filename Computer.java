1d3fc99492dbdea31621b4c0dbc7b146a3a97b51
kyleoleksiuk <kyleoleksiuk@gmail.com>
2014-01-19 02:42:38 -0500

    Update Computer.java

--- Computer.java
+++ Computer.java
@@ -1,9 +1,28 @@
+import java.util.ArrayList;
+
 public class Computer extends Player{
-  
-  public Card playCard(){
-    
-    
-    
+
+	public Computer() {
+    _name = "";
+    _score = 0;
+    _roundScore = 0;
+    _hand = new ArrayList<Card>();
+    isLeading = false;
+    isBroken = false;
   }
-  
+
+  public Computer(String name) {
+    _name = name;
+    _score = 0;
+    _roundScore = 0;
+    _hand = new ArrayList<Card>();
+    isLeading = false;
+    isBroken = false;
   }
+	
+	public Card playCard(){//do in morning
+		Card h = new Card(3,1);
+		return h;
+  	}
+  
+}
