110e910c8ad0f53cdcb41fcf02ba105339dd2bd3
kyleoleksiuk <kyleoleksiuk@gmail.com>
2014-01-19 02:39:03 -0500

    Update Human.java

--- Human.java
+++ Human.java
@@ -1,6 +1,25 @@
+import java.util.ArrayList;
 import cs1.Keyboard;
 
 public class Human extends Player{
+
+  public Human() {
+    _name = "";
+    _score = 0;
+    _roundScore = 0;
+    _hand = new ArrayList<Card>();
+    isLeading = false;
+    isBroken = false;
+  }
+
+  public Human(String name) {
+    _name = name;
+    _score = 0;
+    _roundScore = 0;
+    _hand = new ArrayList<Card>();
+    isLeading = false;
+    isBroken = false;
+  }
   
   public String isOkay(int h){
     
@@ -12,7 +31,7 @@ public class Human extends Player{
     if ( isLeading == true && isBroken == false && d.getSuit() == 0) {retStr = "Illegal move. Hearts has not yet been broken, please lead a card of another suit.";}
     if (isLeading == false && _trickSuit != d.getSuit()){retStr = "Illegal move. Please play a " + d.getSuitName() + ".";} //make getSuitName
     
-    return retStr;   
+    return retStr;
   }
 
   public boolean inHand(Card d){
@@ -32,16 +51,16 @@ public class Human extends Player{
     showHand();
     //include catchException in case doubles/floats/non-numbers are inputted
     System.out.println("It's your turn, please select a card using an integer from 1 to " + _hand.size()+ ".");
-    int h = Keyboard.getInt();
+    int h = Keyboard.readInt();
     
     while (isOkay(h)!= ""){
       System.out.println(isOkay(h));
-      int h = Keyboard.getInt();
+      h = Keyboard.readInt();
     }
     
     System.out.println(isOkay(h));
     Card retCard = _hand.get(h-1);
-    return retCard
+    return retCard;
   }
   
 }
