61d297080d9481219fd15f8c481fd5acff025cbe
kyleoleksiuk <kyleoleksiuk@gmail.com>
2014-01-18 23:16:54 -0500

    Update Human.java

--- Human.java
+++ Human.java
@@ -1,12 +1,16 @@
+import cs1.Keyboard;
+
 public class Human extends Player{
   
-  public String isOkay(Card d){
+  public String isOkay(int d){
+    
+    if (d < 1 || d > _hand.size()){return "Illegal move. Please select a card in your hand.";}
+    Card d = _hand.get(d);
     
     String retStr = "";
     
-    if ( isLeading == true && isBroken == false) {retStr = "Illegal move. Hearts has not yet been broken, please lead a card of another suit.";}
+    if ( isLeading == true && isBroken == false && d.getSuit() == 0) {retStr = "Illegal move. Hearts has not yet been broken, please lead a card of another suit.";}
     if (isLeading == false && _trickSuit != d.getSuit()){retStr = "Illegal move. Please play a " + d.getSuitName() + ".";} //make getSuitName
-    if (inHand(d) == false){retStr = "Illegal move. Please select a card in your hand.";}
     
     return retStr;   
   }
@@ -20,6 +24,23 @@ public class Human extends Player{
     return false;
   }
   
-  //playCard() method
+  public void showHand(){
+    System.out.println(getHand());
+  }
+  
+  public Card playCard(){
+    showHand();
+    //include catchException in case doubles/floats/non-numbers are inputted
+    System.out.println("It's your turn, please select a card using an integer from 1 to " + _hand.size()+ ".");
+    int h = Keyboard.getInt();
+    
+    while (isOkay(h)!= ""){
+      System.out.println(isOkay(h));
+      retCard = findCard(Keyboard.getString());
+    }
+    
+    System.out.println(isOkay(h));
+    return retCard
+  }
   
 }
