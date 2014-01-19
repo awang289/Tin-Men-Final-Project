5d12d819cce48557bb33427e66878a8801c25262
kyleoleksiuk <kyleoleksiuk@gmail.com>
2014-01-19 02:36:54 -0500

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,171 +1,206 @@
 //All we need is methods to print the score and the winner at the end of the game and the driver is DONE!
 import cs1.Keyboard;
+import java.util.ArrayList;
 
 public class Driver{
 
-	private static ArrayList<Card> _deck;
+        private static ArrayList<Card> _deck;
 
-	private static ArrayList<Card> _table
-	
-	private static int leading;
-	
-	private static ArrayList<Player> players = new ArrayList<Player>();
-	
-	private static int trickSuit;
-	
-	
-	public static String getTrick() { return trickSuit; }
-	public static void newDeck(){
-		_deck = new ArrayList<Card>();
-		for (int x = 0; x < 4 ; x++){
+        private static ArrayList<Card> _table;
+        
+        private static int leading;
+        
+        private static ArrayList<Player> players = new ArrayList<Player>();
+        
+        private static int trickSuit;
+        
+        public static int getTrick() { return trickSuit; }
 
-			for (int h = 0; h < 13 ; h++){
+        public static void printScore(){
+                String printStr = "==================================";
+                printStr += "SCORES:";
+                for (int x = 0; x < players.size(); x++){
+                        printStr += players.get(x);
+                }
+                printStr = "==================================";
+                System.out.println(printStr);
+        }
 
-				Card g = new Card(x,h);
+        public static void printWinner(){
+                String printStr = "==================================";
+                printStr += "WINNER(S):";
+                int lowest = 100;
+                String g = "";
+                for (int x = 0; x < players.size(); x++){
+                        if (players.get(x).getScore() < lowest){
+                                g = players.get(x).getName();
+                        }
+                        if (players.get(x).getScore() == lowest){
+                                g += ", " + players.get(x).getName();
+                        }
+                }
+                printStr = "==================================";
+                System.out.println(printStr);
+        }
 
-				_deck.add(g);
+        public static void newDeck(){
 
-			}
+                _deck = new ArrayList<Card>();
+                for (int x = 0; x < 4 ; x++){
 
-		}
+                        for (int h = 0; h < 13 ; h++){
 
-	}
+                                Card g = new Card(x,h);
 
-	public static void dealHand(Player j){
-		
-		while (int x = 0; x < 13 ; x++){
-			int h = (int) (Math.random()*_deck.size());
-			j.add(_deck.remove(_deck.get(h)));
-		}
+                                _deck.add(g);
 
-	}
-	
-	public static void startRound() {
-		newDeck();
-		for (int x = 0; x < 4; x++) {
-			dealHand(players.get(x))
-			players.get(x).sort();
-		}
-	}
-	
-	public static boolean anyLosers() {	
-		return ( (p1.getScore() >= 100 ) || ( p2.getScore() >= 100 ) || 
-			(p3.getScore() >= 100 ) || ( p4.getScore() >= 100 ) ) ;
-	}
-	public static boolean checkStart( Player p ) {
-		Card C3 = new Card(3,1)
-		for (int x = 0; x < 13; x++) {
-			if (p.get(x).compareTo( C3) == 0)
-				return true;
-		}
-		return false;	
-		
-	}
-	public static void setLeading (int n) {
-		for (int x = 0; x < 4; x++)
-			players.get(x).setLeading(false);
-		players.get(n).setLeading(true);
-	}
-	public static void setTrick (Card c) {
-		for (int x = 0; x < 4; x++)
-			players.get(x).setTrickSuit(c.getSuit());
-		trickSuit = c.getSuit();
-	}
-	public static void tallyTrick(Card c1, Card c2, Card c3, Card c4) {
-		Card max = c1;
-		if (c2.getSuit == trickSuit && c2.compareTo(c1) > 0)
-			max = c2;
-		if (c3.getSuit == trickSuit && c3.compareTo(c1) > 0)
-			max = c3;
-		if (c4.getSuit == trickSuit && c4.compareTo(c1) > 0)
-			max = c4;
-		int total = c1.getValue() + c2.getValue() + c3.getValue() + c4.getValue();
-		if (max.equals(c1))
-			players.get(leading).addPoints(total);
-		else if (max.equals(c2))
-			players.get( (leading + 1) % 4).addPoints(total);
-			leading = (leading + 1) % 4;
-		else if (max.equals(c3))
-			players.get( (leading + 2) % 4).addPoints(total);
-			leading = (leading + 2) % 4;
-		else
-			players.get( (leading + 3) % 4).addPoints(total);
-		leading = (leading + 1) % 4;
-		if (total > 0) {
-			for (int x = 0; x < 4; x++) 
-				players.get(x).setBroken(true);
-		}
-	}
-	public static void tallyRound();
-		if (players.get(0).getRound() == 26 || players.get(1).getRound() == 26 || 
-			players.get(2).getRound() == 26 || players.get(3).getRound() == 26) {
-			for (int x = 0; x < 4; x++) {
-				if (players.get(x).getRound() != 26)
-					players.get(x).addPoints(26);
-				else 
-					players.get(x).addPoints(-26);
-			}
-		}
-		players.get(0).tally();
-		players.get(1).tally();
-		players.get(2).tally();
-		players.get(3).tally();
-	public static Card playTurn( Player m ) {
-		return m.playCard();
-	}		
-	public static void main(String [] args){
-		String j;
-		int s;
-		Card c1, c2, c3, c4;
-		System.out.print("What is your name? ");
-		j = Keyboard.readString();
-		Player p1 = new Human(j);
-		Player p2 = new Computer("West");
-		Player p3 = new Computer("North");
-		Player p4 = new Computer("East");
-		System.out.print("1. Easy \n2. Medium \n3. Hard \nChoose your difficulty level: ");
-		s = Keyboard.readInt();
-		p2.setDifficulty(s);
-		p3.setDifficulty(s);
-		p4.setDifficulty(s);
-		if (checkStart(p1))
-			leading = 0;
-		else if (checkStart(p2))
-			leading = 1;
-		else if (checkStart(p3))
-			leading = 2;
-		else 
-			leading = 3;
-		players.add(p1);
-		players.add(p2);
-		players.add(p3);
-		players.add(p4);
-		
+                        }
+
+                }
+
+        }
+
+        public static void dealHand(Player j){
+                
+                for (int s = 0; s < 13 ; s++){
+                        int h = (int) (Math.random()*_deck.size());
+
+                        Card d = _deck.get(h);
+                        _deck.remove(d);
+                        j.add(0, d);
+                }
+
+        }
+        
+        public static void startRound() {
+                newDeck();
+                for (int x = 0; x < 4; x++) {
+                        dealHand(players.get(x));
+                        players.get(x).sort();
+                }
+        }
+        
+        public static boolean anyLosers(Player p1, Player p2, Player p3, Player p4) {        
+                return ( (p1.getScore() >= 100 ) || ( p2.getScore() >= 100 ) ||
+                        (p3.getScore() >= 100 ) || ( p4.getScore() >= 100 ));
+        }
+        public static boolean checkStart( Player p ) {
+                Card C3 = new Card(3,1);
+                for (int x = 0; x < p.retHand().size(); x++) {
+                        if (p.retHand().get(x).compareTo(C3) == 0)
+                                return true;
+                }
+                return false;        
+                
+        }
+        public static void setLeading (int n) {
+                for (int x = 0; x < 4; x++)
+                        players.get(x).setLeading(false);
+                players.get(n).setLeading(true);
+        }
+        public static void setTrick (Card c) {
+                for (int x = 0; x < 4; x++)
+                        players.get(x).setTrickSuit(c.getSuit());
+                trickSuit = c.getSuit();
+        }
+        public static void tallyTrick(Card c1, Card c2, Card c3, Card c4) {
+                //redo this later, use no inputs and a for loop referencing the ArrayList players
+                Card max = c1;
+                if (c2.getSuit() == trickSuit && c2.compareTo(c1) > 0)
+                        max = c2;
+                if (c3.getSuit() == trickSuit && c3.compareTo(c1) > 0)
+                        max = c3;
+                if (c4.getSuit() == trickSuit && c4.compareTo(c1) > 0)
+                        max = c4;
+                int total = c1.getValue() + c2.getValue() + c3.getValue() + c4.getValue();
+                if (max.equals(c1))
+                        {players.get(leading).addPoints(total);}
+                else if (max.equals(c2))
+                        {players.get( (leading + 1) % 4).addPoints(total);
+                        leading = (leading + 1) % 4;}
+                else if (max.equals(c3))
+                        { players.get( (leading + 2) % 4).addPoints(total);
+                        leading = (leading + 2) % 4;}
+                else
+                        {players.get( (leading + 3) % 4).addPoints(total);}
+                leading = (leading + 1) % 4;
+                if (total > 0) {
+                        for (int x = 0; x < 4; x++)
+                                players.get(x).setBroken(true);
+                }
+        }
+
+        public static void tallyRound(){
+                if (players.get(0).getRound() == 26 || players.get(1).getRound() == 26 ||
+                        players.get(2).getRound() == 26 || players.get(3).getRound() == 26) {
+                        for (int x = 0; x < 4; x++) {
+                                if (players.get(x).getRound() != 26)
+                                        players.get(x).addPoints(26);
+                                else
+                                        players.get(x).addPoints(-26);
+                        }
+                }
+                players.get(0).tally();
+                players.get(1).tally();
+                players.get(2).tally();
+                players.get(3).tally();
+        }
+
+        public static Card playTurn( Player m ) {
+                return m.playCard();
+        }                
+        public static void main(String [] args){
+                int s;
+                Card c1, c2, c3, c4;
+                System.out.print("What is your name?");
+                String j = Keyboard.readString();
+                Player p1 = new Human(j);
+                Player p2 = new Computer("West");
+                Player p3 = new Computer("North");
+                Player p4 = new Computer("East");
+                System.out.print("1. Easy \n2. Medium \n3. Hard \nChoose your difficulty level: ");
+                s = Keyboard.readInt();
+                p2.setDifficulty(s);
+                p3.setDifficulty(s);
+                p4.setDifficulty(s);
+                if (checkStart(p1))
+                        leading = 0;
+                else if (checkStart(p2))
+                        leading = 1;
+                else if (checkStart(p3))
+                        leading = 2;
+                else
+                        leading = 3;
+                players.add(p1);
+                players.add(p2);
+                players.add(p3);
+                players.add(p4);
+                
 
 
-		while ( anyLosers() == false ){
-			//dealHand to all players
-			//playRound() -- with each play, add cards to onTable
-			//add all cards in onTable back to deck, remove from onTable
-			//reevaluate anyLosers
-			startRound();
-			//set first person
-			for (int x = 0; x < 13; x++) {
-				setLeading(leading);
-				c1 = playTurn(players.get( leading ));
-				setTrick(c1);
-				c2 = playTurn(players.get( (leading + 1) % 4);
-				c3 = playTurn(players.get( (leading + 2) % 4);
-				c4 = playTurn(players.get( (leading + 3) % 4);
-				tallyTrick(c1, c2, c3, c4);
+                while ( anyLosers(p1,p2,p3,p4) == false ){
+                        //dealHand to all players
+                        //playRound() -- with each play, add cards to onTable
+                        //add all cards in onTable back to deck, remove from onTable
+                        //reevaluate anyLosers
+                        startRound();
+                        //set first person
+                        for (int x = 0; x < 13; x++) {
+                                setLeading(leading);
+                                c1 = playTurn(players.get( leading ));
+                                setTrick(c1);
+                                c2 = playTurn(players.get( (leading + 1) % 4));
+                                c3 = playTurn(players.get( (leading + 2) % 4));
+                                c4 = playTurn(players.get( (leading + 3) % 4));
+                                tallyTrick(c1, c2, c3, c4);
 
-			}
-			tallyRound();
-			printScore();
-		}
-		printWinner();
+                        }
+                        tallyRound();
+                        printScore();
+                }
+                printWinner();
 
-	}
+        }
 
 
 }
