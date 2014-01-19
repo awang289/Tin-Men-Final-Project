4cd6bcb36c4446190f4983863cd141595a1f9094
kyleoleksiuk <kyleoleksiuk@gmail.com>
2014-01-19 12:45:24 -0500

    Update Computer.java

--- Computer.java
+++ Computer.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class Computer extends Player{
 
-	public Computer() {
+  public Computer() {
     _name = "";
     _score = 0;
     _roundScore = 0;
@@ -19,10 +19,34 @@ public class Computer extends Player{
     isLeading = false;
     isBroken = false;
   }
+  
+  
+  public boolean checkSuit(){
+  	for (int x = 0; x < _hand.size(); x++){
+  		if (_hand.get(x).getSuit()==getTrick()){
+  			return true;
+  		}
+  	}
+  	
+  	return false;
+  }
+  
 	
-	public Card playCard(){//do in morning
-		Card h = new Card(3,1);
-		return h;
+  public Card playCard(){//do in morning
+    ArrayList<Card> toUse = _hand;
+    boolean anyOfSuit = checkSuit(); 
+    for (int x = 0; x < _toUse.size(); x++){
+    	if (isLeading == true && isBroken == false && toUse.get(x).getSuit() == 0){
+    		toUse.remove(x);
+    		x--;
+    	}
+    	if (toUse.get(x).getSuit()!= getTrick() && anyOfSuit == true && isLeading == false){
+    		toUse.remove(x);
+    		x--;
+    	}
+    }
+    //at this point we have an arraylist toUse of all legal cards
+    return h; //h will be final outputted card
   	}
   
 }
