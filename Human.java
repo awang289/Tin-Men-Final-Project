9c7a400acb55ee021ce229b1ae3d6f14b5c67df6
awang289 <awang289@gmail.com>
2014-01-20 13:14:36 -0500

    Update Human.java

--- Human.java
+++ Human.java
@@ -61,8 +61,7 @@ public class Human extends Player{
     }
     
     System.out.println(isOkay(h,j));
-    Card retCard = _hand.get(h-1);
-    _hand.remove(h-1);
+    Card retCard = _hand.remove(h-1);
     System.out.println(_name + "played " + retCard.toString() + ".");
     return retCard;
   }
